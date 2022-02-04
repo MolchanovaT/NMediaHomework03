088bedada7a337bfa491718d99bddfeff9d53314
Tatyana Molchanova <tatuka.08@mail.ru>
2022-02-04 20:51:52 +0300

    Homework06_recyclerview

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -3,7 +3,8 @@ package ru.netology.nmedia.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.*
+import ru.netology.nmedia.data.Post
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
@@ -14,41 +15,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                author.text = post.author
-                published.text = post.published
-                content.text = post.content
-                likes.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_liked else R.drawable.ic_like
-                )
+        val adapter = PostsAdapter(object : OnInteractionListener{
 
-                likesAmount.text = amountRepresentation(post.likes)
-                repostsAmount.text = amountRepresentation(post.reposts)
-                viewsAmount.text = amountRepresentation(post.views)
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
             }
 
-            binding.likes.setOnClickListener {
-                viewModel.like()
+            override fun onRepost(post: Post) {
+                viewModel.repostById(post.id)
             }
 
-            binding.reposts.setOnClickListener {
-                viewModel.repost()
+            override fun onView(post: Post) {
+                viewModel.viewById(post.id)
             }
-
-            binding.views.setOnClickListener {
-                viewModel.view()
-            }
-        }
-    }
-
-    private fun amountRepresentation(num: Int): String {
-        return when {
-            num < 1_000 -> num.toString()
-            num in 1_000..9999 -> "%.1f".format(num.toDouble() / 1_000) + "K"
-            num in 10_000..999999 -> (num / 1_000).toString() + "K"
-            num >= 1_000_000 -> "%.1f".format(num.toDouble() / 1_000_000) + "M"
-            else -> num.toString()
-        }
+        })
+        binding.list.adapter = adapter
+        viewModel.data.observe(this, adapter::submitList)
     }
 }
--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.LiveData
 import ru.netology.nmedia.data.Post
 
 interface PostRepository {
-    fun get(): LiveData<Post>
-    fun like()
-    fun repost()
-    fun view()
+    fun getAll(): LiveData<List<Post>>
+    fun likeById(id: Long)
+    fun repostById(id: Long)
+    fun viewById(id: Long)
 }
